5823ff604b399e5aa2ce23a826639e7bfe0ce212
mkeemon <keemonm@bc.edu>
2013-07-18 13:20:12 -0400

    quick change to registration_controller_spec

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -12,14 +12,14 @@ describe RegistrationsController do
        response.response_code.should == 422
       end
     end
-    context 'registration success with email confirmation' do
+    context 'registration success' do
       before do
         user = Fabricate.attributes_for(:user)
         user[:password_confirmation] = user[:password]
         xhr :post, :create, :user => user
       end
       it 'returns http 201' do
-        response.response_code.should == 401
+        response.response_code.should == 201
       end
     end
   end
